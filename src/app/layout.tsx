50100a4718a7e4919b02aa0863ea39501ac18944
Rafli Ardian <guekece159@gmail.com>
2025-11-17 07:20:20 +0000

    ini ganti dengan ini https://image2url.com/images/1763362625936-b9cad852

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="https://image2url.com/images/1763362625936-b9cad852-d8c8-4257-80b4-788574a96880.jpeg" type="image/jpeg" />
       </head>
       <body className={cn('font-body antialiased min-h-screen flex flex-col')}>
         <FirebaseClientProvider>
