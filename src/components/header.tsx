d294ff2f97f8b3f13391f5a9ce68b2ed7996ad83
Rafli Ardian <guekece159@gmail.com>
2025-11-17 05:06:53 +0000

    ganti warna putih (_for element div_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
           <Link href="/" className="flex items-center gap-2">
             <span className="font-bold text-base font-headline">PILIHAN YANG TEPAT ADA DI NAURA</span>
           </Link>
-          <div className="font-bold text-destructive text-base">
+          <div className="font-bold text-foreground text-base">
             TIDAK BISA COD
           </div>
         </div>
